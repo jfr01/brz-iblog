fcc6820a330ce320f1ff94e8ad226873f6cd4f58
s0l0d0l0011 <jframonex@gmail.com>
2021-02-11 21:48:25 -0500

    started images expand function with js

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
 const COLUMNS = document.getElementById('column');
+const IMAGE = document.getElementsByClassName('galleryimage');
+//console.log(image);
 
+// EXPAND IMAGE
+function expandImage() {
+    IMAGE.style.transform = "scale(1.5)";
+    IMAGE.style.transition = "transform 0.25s ease";
+}
+
+// RESET IMAGE
+function resetExpandImage() {
+    IMAGE.style.transform = "scale(1)";
+    IMAGE.style.transition = "transform 0.25s ease";
+}
