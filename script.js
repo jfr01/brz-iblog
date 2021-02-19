101a76fc4f6a8cc2bbab30aa5f1607d745984862
s0l0d0l0011 <jframonex@gmail.com>
2021-02-18 22:10:04 -0500

    trying modal effect for img gall

--- script.js
+++ script.js
@@ -1,15 +1,34 @@
-const COLUMNS = document.getElementById('column');
-const IMAGE = document.getElementsByClassName('galleryimage');
+// const COLUMNS = document.getElementById('column');
+// const IMAGE = document.getElementsByClassName('galleryimage');
 //console.log(image);
 
-// EXPAND IMAGE
-function expandImage() {
-    IMAGE.style.transform = "scale(1.5)";
-    IMAGE.style.transition = "transform 0.25s ease";
+// // EXPAND IMAGE
+// function expandImage() {
+//     IMAGE.style.transform = "scale(1.5)";
+//     IMAGE.style.transition = "transform 0.25s ease";
+// }
+
+// // RESET IMAGE
+// function resetExpandImage() {
+//     IMAGE.style.transform = "scale(1)";
+//     IMAGE.style.transition = "transform 0.25s ease";
+// }
+
+
+let modal = document.getElementById('myModal');
+let img = document.getElementById('.galleryimage');
+let modalImg = document.getElementById('img01');
+let captionText = document.getElementById('caption');
+let span = document.getElementsByClassName('close')[0];
+
+
+img.onclick = function () {
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
 }
 
-// RESET IMAGE
-function resetExpandImage() {
-    IMAGE.style.transform = "scale(1)";
-    IMAGE.style.transition = "transform 0.25s ease";
+
+span.onclick = function () {
+    modal.style.display = "none";
 }
