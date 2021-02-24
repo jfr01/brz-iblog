7b93ad7544c7c5153b23a4ee4a50ae785efd4565
s0l0d0l0011 <jframonex@gmail.com>
2021-02-24 18:04:29 -0500

    fixed func to loop thru img gallery

--- script.js
+++ script.js
@@ -1,33 +1,39 @@
-// const COLUMNS = document.getElementById('column');
-// const IMAGE = document.getElementsByClassName('galleryimage');
-//console.log(image);
+// let modal = document.getElementById('myModal');
+// let img = document.getElementsByClassName('galleryimage');
+// let modalImg = document.getElementById('img01');
+// let captionText = document.getElementById('caption');
+// let span = document.getElementsByClassName('close')[0];
 
-// // EXPAND IMAGE
-// function expandImage() {
-//     IMAGE.style.transform = "scale(1.5)";
-//     IMAGE.style.transition = "transform 0.25s ease";
-// }
 
-// // RESET IMAGE
-// function resetExpandImage() {
-//     IMAGE.style.transform = "scale(1)";
-//     IMAGE.style.transition = "transform 0.25s ease";
+// // img.onClick = function () {
+// //     modal.style.display = "block";
+// //     modalImg.src = this.src;
+// //     captionText.innerHTML = this.alt;
+// // }
+
+// img.forEach(e => e.onClick = function () {
+//     modal.style.display = "block";
+//     modalImg.src = this.src;
+//     captionText.innerHTML = this.alt;
+// })
+
+// span.onClick = function () {
+//     modal.style.display = "none";
 // }
 
 
 let modal = document.getElementById('myModal');
-let img = document.getElementsByClassName('galleryimage');
+let images = document.getElementsByClassName('galleryimage');
 let modalImg = document.getElementById('img01');
 let captionText = document.getElementById('caption');
 let span = document.getElementsByClassName('close')[0];
 
 
-img.onClick = function () {
-    modal.style.display = "block";
-    modalImg.src = this.src;
-    captionText.innerHTML = this.alt;
-}
-
-span.onClick = function () {
-    modal.style.display = "none";
-}
+Array.from(images).forEach(function (img) {
+    console.log(img);
+    img.onclick = function () {
+        modal.style.display = "block";
+        modalImg.src = this.src;
+        captionText.innerHTML = this.alt;
+    }
+})
